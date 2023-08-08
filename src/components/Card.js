98a24a16f4bf4097d06ff513b3f8fbb96670453e
Sajal Sharma <sajalsharma2408@gmail.com>
2023-08-09 01:18:57 +0530

    done w/ card functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,37 @@ export default function Card() {
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img
+          src="https://i.pinimg.com/originals/d6/8c/33/d68c333aae7879d9e08bf3e412e7ce84.jpg"
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">hands corner knifeee</p>
+
+          {/* now making the container component */}
+          <div className="container w-100">
+            {/* first, the quantitly select option */}
+            <select className="m-2 h-100 bg-success rounded">
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+
+            {/* now the half/full select options */}
+            <select className="m-2 h-100 bg-success rounded">
+              <option value="half">Half</option>
+              <option value="full">Full</option>
+            </select>
+
+            {/* now the price part */}
+            <div className="d-inline h-100 fs-5">Total Price</div>
+          </div>
         </div>
       </div>
     </div>
