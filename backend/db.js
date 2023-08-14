c80daa11be08e24498e4343331815133eb318218
Sajal Sharma <sajalsharma2408@gmail.com>
2023-08-15 00:54:08 +0530

    first route made - createUser

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ const connectToMongo = async () => {
     console.log("Connected!");
     let fetched_data = mongoose.connection.db.collection("food_items");
     let data = await fetched_data.find({}).toArray();
-    console.log(data);
+    // console.log(data);
   } catch (error) {
     console.log("err: ", error);
   }
--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,14 @@ const connectToMongo = require("./db");
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 connectToMongo();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api", require("./routes/CreateUser"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
